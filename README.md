2ac267184056beebbbe7be565b0dd4ef8a208132
Ante Braovic <abraovic@gmail.com>
2016-01-29 11:41:21 +0100

    examples updated

--- README.md
+++ README.md
@@ -23,10 +23,10 @@ try {
 
     $type = PHPush\Push\Push::IOS;
     $credentials = [
-        'device_token' => 'fake-token',
-        'certificate_path' => 'fake-cert-path',
-        'certificate_phrase' => 'fake-cert-phrase',
-        'dev' => true
+        'device_token' => 'paste-token-here',
+        'certificate_path' => 'exported-cert.pem', // make sure it is pem
+        'certificate_phrase' => 'pass-if-needed',
+        'dev' => true // make sure this meets certificate
     ];
     
     $push = new PHPush\Push\Push($type, $credentials);
--- examples/apns.php
+++ examples/apns.php
@@ -1,2 +1,27 @@
 <?php
 
+require '../vendor/autoload.php';
+
+use abraovic\PHPush;
+use abraovic\PHPush\Exception\PHPushException;
+
+
+try {
+
+    $type = PHPush\Push\Push::IOS;
+    $credentials = [
+        'device_token' => 'paste-token-here',
+        'certificate_path' => 'exported-cert.pem', // make sure it is pem
+        'certificate_phrase' => 'pass-if-needed',
+        'dev' => true // make sure this meets certificate
+    ];
+
+    $push = new PHPush\Push\Push($type, $credentials);
+    $message = new PHPush\iOS\Message();
+    $message->setAlert("This will be shown");
+    $message->setBadge(2);
+    $push->sendMessage($message);
+
+} catch (PHPushException $e) {
+    echo 'Caught exception: ' . $e->getMessage() . "\n";
+}
